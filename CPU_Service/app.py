b3337c2518c9fc8b11cd7c30eaf62522adc5d112
vidit23 <viditvineet@hotmail.com>
2021-03-31 13:09:37 -0400

    Repeatition of tasks

--- CPU_Service/app.py
+++ CPU_Service/app.py
@@ -7,30 +7,33 @@ import hashlib
 
 app = Flask(__name__)
 with open(r'./config.yaml') as file:
-  locationMappings = yaml.load(file, Loader=yaml.FullLoader)
+	locationMappings = yaml.load(file, Loader=yaml.FullLoader)
 
 
 @app.route('/hashFile', methods=['GET'])
 def hash_file():
-  h = hashlib.sha512()
-  with open("./test.mp3", 'rb') as file:
-    # loop till the end of the file
-    chunk = 0
-    while chunk != b'':
-        # read only 1024 bytes at a time
-        chunk = file.read(1024)
-        h.update(chunk)
-  return jsonify({'hash' : h.hexdigest()})
+	repeat = request.args['repeat']
+	for _ in range(repeat):
+		h = hashlib.sha512()
+		with open("./test.mp3", 'rb') as file:
+			# loop till the end of the file
+			chunk = 0
+			while chunk != b'':
+				# read only 1024 bytes at a time
+				chunk = file.read(1024)
+				h.update(chunk)
+	return jsonify({'hash' : h.hexdigest()})
 
 @app.route('/generateLoad', methods=['GET'])
 def generate_multiply_load():
-  for _ in range(10000000):
-      pr = 213123  # generates some load
-      x = pr * pr
-      pr = pr + 1
-  return jsonify({'result': x})
+	repeat = request.args['repeat']
+	for _ in range(repeat):
+		pr = 213123  # generates some load
+		x = pr * pr
+		pr = pr + 1
+	return jsonify({'result': x})
 
 if __name__ == '__main__':
-    host = os.environ['hasherHost']
-    port = os.environ['hasherPort']
-    app.run(host=host, port=port, debug=True)
+	host = os.environ['hasherHost']
+	port = os.environ['hasherPort']
+	app.run(host=host, port=port, debug=True)
--- DB_Service/app.py
+++ DB_Service/app.py
@@ -4,11 +4,12 @@ from flask import request
 from flask_pymongo import PyMongo
 import yaml
 import os
+import random
+import string
 
 app = Flask(__name__)
 with open(r'./config.yaml') as file:
-  locationMappings = yaml.load(file, Loader=yaml.FullLoader)
-
+	locationMappings = yaml.load(file, Loader=yaml.FullLoader)
 
 # app.config['MONGO_DBNAME'] = locationMappings["db"]["name"]
 # app.config['MONGO_URI'] = 'mongodb://' + locationMappings["db"]["url"] + ':' + str(locationMappings["db"]["port"]) + '/'+ locationMappings["db"]["name"]
@@ -20,26 +21,34 @@ mongo = PyMongo(app)
 
 @app.route('/bulkRead', methods=['GET'])
 def get_all_stars():
-  star = mongo.db.stars
-  output = []
-  for s in star.find():
-    output.append({'name' : s['name'], 'updated' : s['updated']})
-  return jsonify({'result' : output})
+	star = mongo.db.stars
+	repeat = request.args['repeat']
+	for _ in range(repeat):
+		letters = string.ascii_lowercase
+		name = ''.join(random.choice(letters) for i in range(10))
+		rank = random.randrange(1000)
+		star.update({"name": name}, {"$set": {'rank': rank}}, upsert = True)
+
+	output = []
+	for s in star.find():
+		output.append({'name' : s['name'], 'rank' : s['rank']})
+	return jsonify({'result' : output})
 
 @app.route('/bulkWrite', methods=['POST'])
 def add_star():
-  star = mongo.db.stars
-  name = request.args['name']
-  times = request.args['times']
-  for i in range(times):
-  	star.update({"name": name}, {"$set": {'updated': i}}, upsert = True)
+	star = mongo.db.stars
+	repeat = request.args['repeat']
+	for _ in range(repeat):
+		letters = string.ascii_lowercase
+		name = ''.join(random.choice(letters) for i in range(10))
+		rank = random.randrange(1000)
+		star.update({"name": name}, {"$set": {'rank': rank}}, upsert = True)
 
-  s = star.find_one({'name' : name})
-  output = {'name' : s['name'], 'updated' : s['updated']}
-  return jsonify({'result' : output})
+	star.remove()
+	return jsonify({'result' : 'Upserted ' + repeat + ' documents in DB and removed all of them'})
 
 if __name__ == '__main__':
-    host = os.environ['writerHost']
-    port = os.environ['writerPort']
-    app.run(host=host, port=port, debug=True)
-    # app.run(host=locationMappings['writer']['url'], port=locationMappings['writer']['port'], debug=True)
+	host = os.environ['writerHost']
+	port = os.environ['writerPort']
+	app.run(host=host, port=port, debug=True)
+	# app.run(host=locationMappings['writer']['url'], port=locationMappings['writer']['port'], debug=True)
