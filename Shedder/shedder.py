46d28065fea702b4d60522ce61ce3fe52b3c4c0b
Jairam Venkit <jairam0@gmail.com>
2021-05-08 02:33:31 -0400

    added new functionality to shedder

--- Shedder/shedder.py
+++ Shedder/shedder.py
@@ -20,6 +20,13 @@ app = Flask(__name__)
 health = HealthCheck()
 envdump = EnvironmentDump()
 
+redisHost = "0.0.0.0"
+redisPort = 6379
+redisPassword = ''
+
+REDIS_PREFIX = "cpu/"
+SLIDING_WINDOW_SIZE = 10
+SERVICE_PORT = ":3000"
 
 with open(r'./config.yaml') as file:
 	locationMappings = yaml.load(file, Loader=yaml.FullLoader)
@@ -30,88 +37,117 @@ def is_running():
 
 health.add_check(is_running)
 
-# Get a list of all the pod IPs periodically or whenenver a new pod appears it calls an api
-@app.route('/api/fetachallpods', methods=['GET'])
-def fetchAllPods():
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    r.set('0.0.0.0:3000', json.dumps({
+# Creates a list of pods in redis at service startup
+@app.route('/api/shed/createpodslist', methods=['GET'])
+def createPodsList():
+    r = redis.Redis(host=redisHost, port=redisPort, password=redisPassword, db=0)
+    contexts, active_context = config.list_kube_config_contexts()
+    if not contexts:
+        print("Cannot find any context in kube-config file.")
+        return
+    print("Context", contexts, "Active context", active_context)
+
+    contexts = [context['name'] for context in contexts]
+    print("Context", contexts)
+
+    active_index = contexts.index(active_context['name'])
+    print("Active Index", active_index)
+
+    cluster1, first_index = pick(contexts, title="Pick the first context",
+                                 default_index=active_index)
+
+    client1 = client.CoreV1Api(
+        api_client=config.new_client_from_config(context=cluster1))
+
+    print("\nList of pods on %s:" % cluster1)
+    for i in client1.list_namespaced_pod(REDIS_PREFIX[:-1]).items:
+        print("%s\t%s\t%s" %
+              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        r.set(REDIS_PREFIX + i.status.pod_ip + SERVICE_PORT, json.dumps({
+            "isActive": True, 
+            "cpu": [],
+            "memory": [],
+            "latency": [],
+            "result": []
+        }))
+
+    keyList = [ key.decode('UTF-8') for key in r.scan_iter(REDIS_PREFIX + "*") ]
+    return jsonify({ "written" : keyList })
+
+
+@app.route('/api/shed/updatepodslist', methods=['POST'])
+def updatePodsList():
+    r = redis.Redis(host=redisHost, port=redisPort, password=redisPassword, db=0)
+    data = request.get_json()
+    ip_addr = data["ip"]
+    r.set(REDIS_PREFIX + ip_addr + SERVICE_PORT, json.dumps({
         "isActive": True, 
         "cpu": [],
         "memory": [],
         "latency": [],
         "result": []
-    }), ex=10)
-    return jsonify({ "written" : list(r.scan_iter("*")) })
-    # contexts, active_context = config.list_kube_config_contexts()
-    # if not contexts:
-    #     print("Cannot find any context in kube-config file.")
-    #     return
-    # contexts = [context['name'] for context in contexts]
-    # active_index = contexts.index(active_context['name'])
-    # cluster1, first_index = pick(contexts, title="Pick the first context",
-    #                              default_index=active_index)
-    # cluster2, _ = pick(contexts, title="Pick the second context",
-    #                    default_index=first_index)
-
-    # client1 = client.CoreV1Api(
-    #     api_client=config.new_client_from_config(context=cluster1))
-    # client2 = client.CoreV1Api(
-    #     api_client=config.new_client_from_config(context=cluster2))
-
-    # print("\nList of pods on %s:" % cluster1)
-    # for i in client1.list_pod_for_all_namespaces().items:
-    #     print("%s\t%s\t%s" %
-    #           (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-
-    # print("\n\nList of pods on %s:" % cluster2)
-    # for i in client2.list_pod_for_all_namespaces().items:
-    #     print("%s\t%s\t%s" %
-    #           (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+    }))
+    keyList = [ key.decode('UTF-8') for key in r.scan_iter(REDIS_PREFIX + "*") ]
+    return jsonify({ "written" :  keyList})
 
 
-@app.route('/api/updatepods', methods=['GET'])
-def updatePodsLocation():
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    r.set('0.0.0.0:3000', json.dumps({
-        "isActive": True, 
-        "cpu": [],
-        "memory": [],
-        "latency": [],
-        "result": []
-    }), ex=10)
-    return jsonify({ "written" : list(r.scan_iter("*")) })
 
 
-# Fetch all the IPs and call healthcheck on them to recieve aliveness, CPU, Memory
-@app.route('/api/healthchecks', methods=['GET'])
+
+# Empty the Redis cache
+@app.route('/api/shed/deletepodslist', methods=['DELETE'])
+def deletePodsList():
+    r = redis.Redis(host=redisHost, port=redisPort, password=redisPassword, db=0)
+    for key in r.scan_iter(REDIS_PREFIX + "*"):
+        r.delete(key)
+    keyList = [ key.decode('UTF-8') for key in r.scan_iter(REDIS_PREFIX + "*") ]
+    return jsonify({ "written" :  keyList})
+
+
+
+
+
+# Fetch all the IPs from Redis and call healthcheck on them to recieve aliveness, CPU, Memory
+@app.route('/api/shed/healthchecks', methods=['GET'])
 def getHealthChecks():
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    for key in r.scan_iter("*"):
+    r = redis.Redis(host=redisHost, port=redisPort, password=redisPassword, db=0)
+    # Iterate through all keys with the REDIS_PREFIX
+    for key in r.scan_iter(REDIS_PREFIX + "*"):
+        # Key is returned in bytes, converting it into a string
         ipAddress = key.decode("utf-8")
+        # Gets the value stored in redis for the particular key, converts it into a JSON object
         pastUsage = json.loads(r.get(ipAddress))
-        print("ipAddressFetch", ipAddress, pastUsage)
+        print("ipAddressFetch", ipAddress[len(REDIS_PREFIX):], pastUsage)
 
         try:
-            usageUpdateResponse = requests.get("http://" + ipAddress  + '/api/cpu/generateLoad?repeat=10', timeout=1).content        
+            # Remove REDIS_PREFIX from key, send request to the resultant IP to fetch the health of the server
+            # endPoint = "http://" + ipAddress[len(REDIS_PREFIX):]  + '/metrics'
+            endPoint = "http://10.4.2.7:3000/metrics"
+            print("Reaching out to " + endPoint)
+            usageUpdateResponse = requests.get(endPoint, timeout=1).content
+            print("Metadata", usageUpdateResponse)
             usageUpdate = dict(json.loads(usageUpdateResponse))
             print("New statistics", usageUpdate)
 
-        except RequestException as e:
-            print("Request didnt work, deleting the node from memory")
-            r.delete(key, json.dumps(pastUsage))
-            return jsonify( {"updatedIps": list(r.scan_iter("*")) })
-
-        for metric in usageUpdate:
-                if metric in pastUsage and len(pastUsage[metric]) > 10:
+            for metric in usageUpdate:
+                if metric in pastUsage and len(pastUsage[metric]) > SLIDING_WINDOW_SIZE:
                     pastUsage[metric].pop(0)
                     pastUsage[metric].append(usageUpdate[metric])
                 elif metric in pastUsage:
                     pastUsage[metric].append(usageUpdate[metric])
-        print("Updated past object", pastUsage)
-            
-        r.set(key, json.dumps(pastUsage), ex=10)
+            print("Updated past object", pastUsage)
+                
+            r.set(key, json.dumps(pastUsage))
 
-    return jsonify( {"updatedIps": list(r.scan_iter("*")) })
+        except RequestException as e:
+            # If the request gives an error, delete key from Redis as the server is unresponsive
+            print("Request didnt work, deleting the node from memory:", e)
+            r.delete(key, json.dumps(pastUsage))
+
+    keyList = [ key.decode('UTF-8') for key in r.scan_iter(REDIS_PREFIX + "*") ]
+    return jsonify( {"updatedIps": keyList })
+
+
 
 # When a new Request coming in
 # Different categories of users * Different categories of APIs
@@ -129,10 +165,20 @@ def requestForwarder(requestType):
 
 if __name__ == '__main__':
 	host = "0.0.0.0"
-	port = 3001
-	if 'hasherHost' in os.environ:
-		host = os.environ['hasherHost']
-	if 'hasherPort' in os.environ:
-		port = os.environ['hasherPort']
+	port = 3004
+	if 'shedderHost' in os.environ:
+		host = os.environ['shedderHost']
+	if 'shedderPort' in os.environ:
+		port = os.environ['shedderPort']
+
+    # Get Redis information
+	if 'redisHost' in os.environ:
+		redisHost = os.environ['redisHost']
+	if 'redisPort' in os.environ:
+		redisPort = os.environ['redisPort']
+	if 'redisPassword' in os.environ:
+		redisPassword = os.environ['redisPassword']
+
+
 	app.add_url_rule("/health", "healthcheck", view_func = lambda: health.run())
 	app.run(host=host, port=port, debug=True)
